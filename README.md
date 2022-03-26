155168088bc445c1dbf69f113fbcba368ba9455f
hfutsora <346762712@qq.com>
2022-03-26 16:28:15 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# vscode-unwrap
+Unwrap selection in [Visual Studio Code](https://github.com/Microsoft/vscode).
+
+## How it works
+
+### Using the command palette
+![Command palette](static/palette.gif)
+
+### Using a keyboard shortcut
+![Keyboard shortcut](static/shortcut.gif)
+
+The default keboard shortcut is set to `alt+u`, but you can change it to something else by overriding the `key` value of the `unwrap.toUnwrap` command.
+
+## License
+MIT
---
+++ src/extension.ts
@@ -0,0 +1,50 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerTextEditorCommand('unwrap.toUnwrap', toUnwrap);
+
+  context.subscriptions.push(disposable);
+}
+
+const wrapPairs = [
+  ['\'', '\''],
+  ['"', '"'],
+  ['{', '}'],
+  ['[', ']'],
+  ['(', ')']
+];
+
+function toUnwrap(editor: vscode.TextEditor) {
+  const replacedSelections: vscode.Selection[] = [];
+
+  editor.edit(builder => {
+    for(const selection of editor.selections) {
+      if(selection.start.character === 0) { continue; }
+
+      const range = new vscode.Range( // create range includes chars surround the selection
+        selection.start.translate({ characterDelta: -1 }), 
+        selection.end.translate({ characterDelta: 1 })
+      );
+      const text = editor.document.getText(range) || '';
+  
+      if(text.length < 3) { return; }
+  
+      const firstChar = text[0];
+      const lastChar = text[text.length - 1];
+  
+      if(wrapPairs.some(pair => firstChar === pair[0] && lastChar === pair[1])) {
+        builder.replace(range, text.slice(1, -1));
+        replacedSelections.push(
+          new vscode.Selection(
+            range.start,
+            range.end.translate({ characterDelta: -1 })
+          )
+        );
+      }
+    }
+  }).then(() => {
+    editor.selections = replacedSelections;
+  });
+}
+
+export function deactivate() {}
